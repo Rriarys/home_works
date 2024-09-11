e48fb6fd246faceadb4b4925cd9e408758838840
Rriarys <rriarys@gmail.com>
2024-09-11 21:05:53 +0300

    Arrays

--- hw2_arrays/Program.cs
+++ hw2_arrays/Program.cs
@@ -23,13 +23,13 @@
         }*/
 
 
-        /*//custom int array
+        //custom int array
 
         while (true)
         {
             Console.Write("Write size of array: "); int array_size = Convert.ToInt32(Console.ReadLine());
 
-            if (array_size <= 0) 
+            if (array_size <= 0)
             {
                 Console.WriteLine("Must be more then 0");
                 Console.Write("Write size of array: "); array_size = Convert.ToInt32(Console.ReadLine());
@@ -41,9 +41,10 @@
             {
                 Console.Write($"Enter {i + 1} element: "); array[i] = Convert.ToInt32(Console.ReadLine());
             }
-
-            Console.Write("Result [");
-            for (int i = 0;i < array_size; i++)
+            
+            //вывод всего массива
+            Console.Write("\nResult [");
+            for (int i = 0; i < array_size; i++)
             {
                 if (i == array_size - 1)
                     Console.Write($"{array[i]}]");
@@ -51,8 +52,34 @@
                     Console.Write($"{array[i]}, ");
             }
 
+            //сумма всех элементов
+            int sum = 0;
+
+            for (int i = 0; i < array_size; i++) 
+                 sum += array[i];
+            
+            Console.WriteLine($"\n\nSum of elements: {sum}");
+
+            //max, min
+            int min = array[0], max = array[0]; //точка отсчета - первый элемент
+
+            for(int i = 0; i < array_size; i++)
+            {
+                if (array[i] < min) //если найдется элемент меньше точки отсчета, он становится новым минимальным
+                    min = array[i];
+
+                if (array[i] > max)
+                    max = array[i];
+            }
+
+            Console.WriteLine($"\nMin: {min}, Max: {max}");
+
+            //average of elements
+            Console.WriteLine($"\nAvarage of elements: {(int)(sum / array.Length)}");
+
+
             Console.WriteLine("\n");
-        }*/
+        }
 
 
 
