a8c4e776a97bacd6f909c8faaf63da60f2e361da
Rriarys <rriarys@gmail.com>
2024-09-11 20:24:28 +0300

    arrays

---
+++ hw2_arrays/Program.cs
@@ -0,0 +1,74 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        /*char[] array = { 'a', 'b', 'c' };
+
+        for (int i = 0; i < array.Length; i++)
+
+            Console.WriteLine(array[i]);*/
+
+
+        /*// 0.1 ... 0.6
+        double[] array = new double[6];
+
+        array[0] = 0.1; Console.Write($"{array[0]}, ");
+
+
+        for (int i = 1; i < array.Length; i++)
+        {
+            array[i] = array[i-1] + 0.1;
+
+            Console.Write($"{array[i]:f1}, ");
+        }*/
+
+
+        /*//custom int array
+
+        while (true)
+        {
+            Console.Write("Write size of array: "); int array_size = Convert.ToInt32(Console.ReadLine());
+
+            if (array_size <= 0) 
+            {
+                Console.WriteLine("Must be more then 0");
+                Console.Write("Write size of array: "); array_size = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int[] array = new int[array_size];
+
+            for (int i = 0; i < array_size; i++)
+            {
+                Console.Write($"Enter {i + 1} element: "); array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("Result [");
+            for (int i = 0;i < array_size; i++)
+            {
+                if (i == array_size - 1)
+                    Console.Write($"{array[i]}]");
+                else
+                    Console.Write($"{array[i]}, ");
+            }
+
+            Console.WriteLine("\n");
+        }*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
