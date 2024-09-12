ea7048d6cfcc14c57dc3a5a353c2ec7fb1267cb0
Rriarys <rriarys@gmail.com>
2024-09-12 16:28:47 +0300

    done 1/2 of new task (array menu)

--- hw2_arrays/Program.cs
+++ hw2_arrays/Program.cs
@@ -23,13 +23,13 @@
         }*/
 
 
-        //custom int array
+        /*//custom int array
 
         while (true)
         {
             Console.Write("Write size of array: "); int array_size = Convert.ToInt32(Console.ReadLine());
 
-            if (array_size <= 0)
+            while (array_size <= 0)
             {
                 Console.WriteLine("Must be more then 0");
                 Console.Write("Write size of array: "); array_size = Convert.ToInt32(Console.ReadLine());
@@ -79,7 +79,104 @@
 
 
             Console.WriteLine("\n");
+        }*/
+
+
+
+        //array menu
+
+        Console.Write("Write sie of array: "); int size_arrray = Convert.ToInt32(Console.ReadLine());
+
+        while (size_arrray <= 0)
+        {
+            Console.WriteLine("Size can be more than 0!");
+            Console.Write("Write sie of array: "); size_arrray = Convert.ToInt32(Console.ReadLine());
         }
+
+        int[] array = new int[size_arrray];
+
+        while (true)
+        {
+            Console.WriteLine("""
+
+            1) - Show array
+            2) - Edit array
+            3) - Add new element
+            4) - Remove element
+
+            """);
+
+            Console.Write("What to do with it: "); int choiсe = Convert.ToInt32(Console.ReadLine());
+
+            switch (choiсe)
+            {
+                case 1: // SHOW
+                    Console.Write("\nYour array: [");
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        if (i == array.Length - 1)
+                            Console.Write($"{array[i]}]\n");
+                        else
+                            Console.Write($"{array[i]}, ");
+                    }
+                    break;
+
+                case 2: // EDIT
+                    Console.Write("\nWhich element you want change, where 0 is index of the first one: "); int index_array = Convert.ToInt32(Console.ReadLine());
+                    
+                    while (index_array < 0 || index_array > array.Length - 1)
+                    {
+                        Console.Write("\nWrong index!\n");
+                        Console.Write("\nWhich element you want change, where 0 is index of the first one: "); index_array = Convert.ToInt32(Console.ReadLine()); 
+                    }
+
+                    Console.Write($"Put new value for {index_array} INDEX: "); array[index_array] = Convert.ToInt32(Console.ReadLine());
+
+                    break;
+
+                case 3: // ADD
+                    Console.Write("\nPut value of new element: "); int new_elem = Convert.ToInt32(Console.ReadLine());
+
+                    int[] tempArray = new int[array.Length + 1]; // делаем новый массив на основе старого + 1
+
+                    for (int i = 0; i < array.Length; i++) // проходим по всем элементам старого массива 
+                        tempArray[i] = array[i];           // и кладем их в новый
+
+                    tempArray[tempArray.Length - 1] = new_elem; // в последний кладем значение юзера
+
+                    array = tempArray; // возвращаем ссылку от нового массива к старому адресу, старый утратит ссылку и будет стерт компилятором
+                    break;
+
+                case 4: // REMOVE
+                    Console.Write("\nWich element you want, where 0 is index of the first one: "); int index_rem = Convert.ToInt32(Console.ReadLine());
+
+                    int[] tempArrayRem = new int[array.Length - 1];
+
+                    for (int i = 0, j = 0; i < array.Length; i++) // i - старый массв, j - новый
+                    {
+                        if (i != index_rem)
+                        {
+                            tempArrayRem[j] = array[i]; // присваеваем если индекс старого массива не подпадает под удаление
+                            j++; // двигаемся по новому
+                        }
+
+                    }
+                    array = tempArrayRem;
+                    break;
+                //new cases
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
 
 
 
