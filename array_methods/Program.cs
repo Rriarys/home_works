acbbaa3a337828aab8bc9f462b8da0aa7a280026
Rriarys <rriarys@gmail.com>
2024-09-15 20:57:37 +0300

    №

---
+++ array_methods/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        int[] array = { 111, 10, 4, 10, 49, 64, 77, 4, 42, 5 };
+
+        Array.Sort(array);
+
+        Console.ReadLine();
+    }
+}
+
